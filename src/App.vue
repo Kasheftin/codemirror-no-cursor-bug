0d6028466055fc66a8d649076c24e09815df3a50
Kasheftin <Kasheftin@gmail.com>
2023-06-07 23:43:11 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Codemirror v-model="code" :extensions="extensions" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Codemirror } from 'vue-codemirror'
+import { oneDark } from '@codemirror/theme-one-dark'
+const code = ref('')
+const extensions = [oneDark]
+</script>
