5a3046cf011e2b646c5e8271e3b6a813609ac249
Kasheftin <Kasheftin@gmail.com>
2023-06-08 11:36:55 +0300

    raw codemirror added, it works correctly

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,50 @@
 <template>
   <div>
-    <Codemirror v-model="code" :extensions="extensions" />
+    <div>codemirror raw: there's a blinking cursor when the content is empty</div>
+    <div ref="editorRawRef" />
+    <div>vue-codemirror: no cursor when the content is empty</div>
+    <Codemirror v-model="code" :extensions="[oneDark]" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 import { Codemirror } from 'vue-codemirror'
-import { oneDark } from '@codemirror/theme-one-dark'
+import { oneDark } from '@codemirror/theme-one-dark';
+
+import { EditorView, basicSetup } from 'codemirror'
+
 const code = ref('')
-const extensions = [oneDark]
+
+const editorRawRef = ref<HTMLDivElement>()
+
+let editorRaw: EditorView | undefined = undefined
+
+onMounted(() => {
+  if (editorRawRef.value) {
+    editorRaw = new EditorView({
+      parent: editorRawRef.value,
+      extensions: [basicSetup, EditorView.updateListener.of((viewUpdate) => {
+        if (viewUpdate.docChanged) {
+          code.value = viewUpdate.state.doc.toString()
+        }
+      }), oneDark]
+    })
+  }
+})
+
+watch(code, (value) => {
+  if (editorRaw) {
+    if (value !== editorRaw.state.doc.toString()) {
+      editorRaw.dispatch({
+        changes: {
+          from: 0,
+          to: editorRaw.state.doc.length,
+          insert: value
+        }
+      })
+    }
+  }
+})
+
 </script>
